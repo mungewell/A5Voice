2d0cf1846e28d3e600dc8ec90f53b43ac4c4ae1e
Simon Wood <simon@mungewell.org>
2024-06-18 18:07:34 -0600

    Fixed, but very slow with USB timeouts

--- retrieve_voice.py
+++ retrieve_voice.py
@@ -1,26 +1,34 @@
 
 # modified implementation of 'TCP' checksum
 
-def checksum(msg):
+def carry_around_add(a, b):
+    c = a + b
+    return (c & 0xffff) + (c >> 16) 
+
+def checksum(msg, init=0x01):
     l = len(msg)
     out = bytearray(msg)
 
     # preset checksum
     out[8] = 0x00
-    out[9] = 0x00
+    out[9] = init
 
     s = 0
     if l & 1:
         out += b"\x00"
 
+    '''
+    # Method 1
+    # fails with
+    # b"\x02\x06\x08\x00\x93\x7d\x00\x00\xfe\xff\x56\x3a\x16\x41\xf6\x01\x00\x00",
     for i in range(0,l,2):
-        s = s +((out[i]<<8)+(out[i+1]))
-
-    s = s + (s >>16) + 1
-    if (s & 0xF000) == 0:
-        s -= 1
-        if (s & 0xF00) == 0xF00:
-            s += 1
+        s = s +((out[i]<<8)+(out[i+1])) # Big Endian
+    s = s + (s >>16)
+    '''
+    # Method 2
+    for i in range(0,l,2):
+        w = (out[i]<<8)+out[i+1]        # Big Endian
+        s = carry_around_add(s, w)
 
     # embed new checksum
     out[8] = ~s & 0xff
@@ -41,6 +49,9 @@ def main():
     usage = "usage: %prog [options] FILENAME"
     parser = OptionParser(usage)
 
+    parser.add_option("-a", "--add",
+        dest="add", type=int, default=0x40200000,
+        help="force the add of the segment to be read")
     parser.add_option("-s", "--size",
         dest="size", type=int,
         help="force the size of the segment to be read")
@@ -96,51 +107,66 @@ def main():
             usb.util.ENDPOINT_IN)
 
     if ep:
-        #ep.write(b"\x01\x03\x03\x00\x00\x00\x00\x00\x86\x38\x62\x79\x65")
-        ep.write(checksum(b"\x01\x03\x03\x00\x00\x00\x00\x00CSbye"))
+        print("Writing data to file:", args[0])
+        outfile = open(args[0], "wb")
+        if not outfile:
+            print("Unable to open FILE for writing")
 
-        try:
-            data = rp.read(64)
-        except (usb.core.USBError, ep):
-            pass
+        init = 1
+        seq = 0x0002
+        add = options.add
+        todo = 0x01600000       # default, changed later
 
-        # hello
-        #ep.write(b"\x02\x00\x05\x00\x01\x00\x00\x00\x2c\xb4\x68\x65\x6c\x6c\x6f")
-        ep.write(checksum(b"\x02\x00\x05\x00\x01\x00\x00\x00CShello"))
+        reset = 2
+        while todo > 0:
+            if reset > 0:
+                #ep.write(b"\x01\x03\x03\x00\x00\x00\x00\x00\x86\x38\x62\x79\x65")
+                ep.write(checksum(b"\x01\x03\x03\x00\x00\x00\x00\x00CSbye"))
 
-        try:
-            data = rp.read(64)
-        except (usb.core.USBError, ep):
-            pass
+                try:
+                    data = rp.read(64)
+                except (usb.core.USBError, ep):
+                    pass
 
-        if data:
-            if options.verbose:
-                print(bytes(data[10:]).decode("ascii"))
+                #ep.write(b"\x02\x00\x05\x00\x01\x00\x00\x00\x2c\xb4\x68\x65\x6c\x6c\x6f")
+                ep.write(checksum(b"\x02\x00\x05\x00\x01\x00\x00\x00CShello"))
 
-            # auto-magically determine segment size
-            if not options.size:
-                #options.size = 0x01600000
-                print("Forcing short read as Checksum() needs fixing...")
-                options.size = 74296
+                try:
+                    data = rp.read(64)
+                except (usb.core.USBError, ep):
+                    pass
 
-            print("Writing data to file:", args[0])
-            outfile = open(args[0], "wb")
-            if not outfile:
-                print("Unable to open FILE for writing")
-            else:
-                seq = 2
-                add = 0x40200000
-                todo = options.size
+                if data and options.verbose:
+                    print(bytes(data[10:]).decode("ascii"))
+
+                if reset == 2:
+                    # auto-magically determine segment size
+                    if not options.size:
+                        options.size = 0x01600000
+
+                    todo = options.size
+
+                reset = 0
 
                 while todo > 0:
                     #ep.write(b"\x02\x06\x08\x00\x02\x00\x00\x00\xb6\xdd\x00\x00\x20\x40\xf6\x01\x00\x00")
                     ep.write(checksum(b"\x02\x06\x08\x00"+struct.pack("<l", seq)+b"CS" \
-                            +struct.pack("<l", add)+struct.pack("<l", 502)))
+                            +struct.pack("<l", add)+struct.pack("<l", 502), init))
 
                     # trying to read 512 bytes through 64 byte port...
                     for i in range(8):
+                        if i == 0 and options.verbose:
+                            print("0x%8.8x : " % add, end='')
+
                         try:
                             data = rp.read(64)
+                        except usb.core.USBTimeoutError:
+                            if options.verbose:
+                                print()
+                            print("Timeout at 0x%8.8x -> Toggling 'init'" % add)
+                            init = (init + 1) & 1
+                            reset = 1
+                            break
                         except (usb.core.USBError, ep):
                             pass
 
@@ -161,20 +187,17 @@ def main():
                                 else:
                                     outfile.write(out[:todo])
                                     todo = 0
-    
-                    # update counters
-                    if seq & 0xf == 0xf:
-                        seq = (seq & 0xfff0) + 0x100
-                        add += 0x0001D896
-                    else:
+
+                    if reset == 0:
+                        # update counters
                         seq += 1
                         add += 0x01f6
+                        if options.verbose:
+                            print("")
+                    else:
+                        break
 
-                    if options.verbose:
-                        print("")
-
-                outfile.close()
-
+        outfile.close()
 
         #ep.write(b"\x02\x03\x03\x00\x0b\x00\x00\x00\x82\x28\x62\x79\x65")
         ep.write(checksum(b"\x02\x03\x03\x00"+struct.pack("<l", seq)+b"CSbye"))
